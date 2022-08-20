a216d839980573656c8eb36bfacf1b8abbbadbe5
Abhijit Kunjiraman <95897040+Abhijitrk97@users.noreply.github.com>
2022-08-20 00:22:45 -0400

    try catch and logging

--- asin_from_url.py
+++ asin_from_url.py
@@ -1,25 +1,41 @@
-
 from sys import flags
-import requests
 import re
+from logfunc import logfunc
 
-###Amazon URL
+logfunc('script start','asin from url',200)
+
 def getasin(href):
     a='Invalid'
     asin = re.search(r'/[dg]p/([^/]+)', href, flags=re.IGNORECASE)
-    asin=asin.group(1)
+    try:
+        asin=asin.group(1)
+    except Exception as e:
+        print('Invalid link unable to scrape asin')
+        logfunc('invalid link','asin from url',400)
+        return a
+    logfunc('asin has been scraped','asin from url',200)
     print(asin)
     if asin != 'slredirect':
+        logfunc('valid asin','asin from url',200)
         return asin
     else:
+        logfunc('invalid asin','asin from url',400)
         return a
 
 def getasin1(href):
     a='Invalid'
     asin = re.search(r'dp/([a-zA-Z]+([0-9]+[a-zA-Z]+)+)\?', href, flags=re.IGNORECASE)
-    asin=asin.group(1)
+    try:
+        asin=asin.group(1)
+    except Exception as e:
+        print('Invalid link unable to scrape asin')
+        logfunc('invalid link','asin from url',400)
+        return a
+    logfunc('asin has been scraped','asin from url',200)
     print(asin)
     if asin != 'slredirect':
+        logfunc('valid asin','asin from url',200)
         return asin
     else:
+        logfunc('invalid asin','asin from url',400)
         return a
