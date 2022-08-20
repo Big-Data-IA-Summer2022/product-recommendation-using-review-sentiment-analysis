c5fc3cf73296bedb240dfc16c2e32afd49880984
Abhijit Kunjiraman <95897040+Abhijitrk97@users.noreply.github.com>
2022-08-20 00:24:38 -0400

    Update product_rating_final.py

--- product_rating_final.py
+++ product_rating_final.py
@@ -3,13 +3,19 @@ from asin_from_url import *
 import requests
 import re
 import pandas as pd
+from logfunc import logfunc
 
 def get_rating_for_product(url: str,y: int):
-    if len(getasin(url))==10:
-        asin=(getasin(url))
-    elif len(getasin1(url))==10:
-        asin=getasin1(url)
-    print(asin)
+    logfunc('script start','product_rating_final',200)
+    try:
+        if len(getasin(url))==10:
+            asin=(getasin(url))
+        elif len(getasin1(url))==10:
+            asin=getasin1(url)
+        print(asin)
+    except Exception as e:
+        logfunc('Unable to get asin','product_rating_final',400)
+        return 'Unable to find reviews due to invalid asin'
     p=0
     n=0
     brokenapi=0
@@ -18,6 +24,7 @@ def get_rating_for_product(url: str,y: int):
         url=f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber={x}"
         print("Downloading %s"%url)
         df=get_reviews(url)
+        logfunc('Scraping reviews','product_rating_final',200)
         print(df)
         if df.empty:
             pass
@@ -38,8 +45,8 @@ def get_rating_for_product(url: str,y: int):
     if p+n!=0:
         result=((p-n)/(p+n))*100
         print(result)
+        logfunc('successfully generated ratings','product_rating_final',200)
     elif p+n==0:
         result='No reviews'
         print('No reviews')
     return result
-
